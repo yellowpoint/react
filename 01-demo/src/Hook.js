1cf7c6576c14cee5a4cae3440141a795234fd952
yellowpoint <870003719@qq.com>
2021-01-29 17:26:58 +0800

    优化 整理结构

---
+++ 01-demo/src/Hook.js
@@ -0,0 +1,5 @@
+const Hook = ()=>{
+  return ( <div>我是Hook组件</div> )
+}
+
+export default Hook
---
+++ 01-demo/src/utils.js
@@ -0,0 +1,44 @@
+export function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+export function calculateWinnerLine(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i];
+    }
+  }
+  return [];
+}
+export function getPosition(index, size = 3) {
+  index += 1
+  const x = index % size ? index % size : size
+  const y = Math.ceil(index / size)
+  return `${y}行,${x}列`
+}
