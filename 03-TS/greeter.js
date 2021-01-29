844180a12fb973479171b7e63f82d0e4c081325b
yellowpoint <870003719@qq.com>
2021-01-29 17:07:41 +0800

    新增 ts练习

---
+++ 03-TS/greeter.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "你好呀，" + person;
+}
+// let user = '大典'
+var user = [1, 2, 3];
+document.body.innerHTML = greeter(user);
---
+++ 03-TS/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+  return "你好呀，" + person
+}
+// let user = '大典'
+let user = [1, 2, 3]
+
+document.body.innerHTML = greeter(user)
