ea21e4a28651c9574abc9e21a78bdc3842f1507e
yellowpoint <870003719@qq.com>
2021-03-02 22:00:06 +0800

    chore: 添加ts类型定义

---
+++ 05-jira/src/utils/index.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
+
+export const cleanObject = (obj: object) => {
+  const result = { ...obj };
+  Object.keys(obj).forEach((key) => {
+    // @ts-ignore
+    if (isFalsy(obj[key])) delete result[key];
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+// export const debounce = (fn, delay) => {
+//   let timeout;
+//   return (...rest) => {
+//     if (timeout) clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       fn(...rest);
+//     }, delay);
+//   };
+// };
+
+// export const throttle = (fn, wait) => {
+//   let prev = new Date();
+//   return (...rest) => {
+//     const now = new Date();
+//     if (now - prev > wait) {
+//       fn(...rest);
+//       prev = new Date();
+//     }
+//   };
+// };
+
+export const useDebounce = (value: any, delay?: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    // 在执行当前 effect 之前对上一个 effect 进行清除s
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
