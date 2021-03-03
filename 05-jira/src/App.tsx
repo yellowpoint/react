e30e54cd9d1e4788a46f4d48edd1b533536e5230
yellowpoint <870003719@qq.com>
2021-03-03 21:07:44 +0800

    feat: 新增login页面与接口中间层

--- 05-jira/src/App.tsx
+++ 05-jira/src/App.tsx
@@ -1,10 +1,12 @@
-import { ProjectListScreen } from "screens/project-list";
+// import { ProjectListScreen } from "screens/project-list";
 import "./App.css";
+import { LoginScreen } from "./screens/login/index";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
+      <LoginScreen></LoginScreen>
+      {/* <ProjectListScreen /> */}
     </div>
   );
 }
