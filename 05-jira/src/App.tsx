62264344dacca12de4ec619b9d3515b47103d4d8
yellowpoint <870003719@qq.com>
2021-03-10 21:50:12 +0800

    feat: 使用useContext添加登录注册系统；

--- 05-jira/src/App.tsx
+++ 05-jira/src/App.tsx
@@ -1,12 +1,14 @@
-// import { ProjectListScreen } from "screens/project-list";
+import React from "react";
 import "./App.css";
-import { LoginScreen } from "./screens/login/index";
+import { useAuth } from "context/auth-context";
+import { AuthenticatedApp } from "./authenticated-app";
+import { UnauthenticatedApp } from "unauthenticated-app";
 
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      <LoginScreen></LoginScreen>
-      {/* <ProjectListScreen /> */}
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
--- 05-jira/src/index.tsx
+++ 05-jira/src/index.tsx
@@ -1,19 +1,21 @@
-import { loadDevTools } from "jira-dev-tool";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
+import { AppProviders } from "context";
 
 loadDevTools(() =>
   ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <AppProviders>
+        <App />
+      </AppProviders>
     </React.StrictMode>,
     document.getElementById("root")
   )
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
