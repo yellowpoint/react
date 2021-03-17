adafa284608a3c004c14da1d23db2a884e92e216
yellowpoint <870003719@qq.com>
2021-03-17 16:12:29 +0800

    style: 安装emotion，优化登录注册页面

--- 05-jira/src/unauthenticated-app/index.tsx
+++ 05-jira/src/unauthenticated-app/index.tsx
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import { RegisterScreen } from "unauthenticated-app/register";
 import { LoginScreen } from "unauthenticated-app/login";
-import { Card } from "antd";
+import { Card, Divider } from "antd";
+import styled from "@emotion/styled";
 
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Card>
+    <Container>
+      <ShadowCard>
         {isRegister ? <RegisterScreen /> : <LoginScreen />}
-        <button onClick={() => setIsRegister(!isRegister)}>
+        <Divider />
+        <a onClick={() => setIsRegister(!isRegister)}>
           切换到{isRegister ? "登录" : "注册"}
-        </button>
-      </Card>
-    </div>
+        </a>
+      </ShadowCard>
+    </Container>
   );
 };
+
+const ShadowCard = styled(Card)`
+  width: 40rem;
+  min-height: 56rem;
+  padding: 3.2rem 4rem;
+  box-sizing: border-box;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
